dc37339704880a050ceaf6c178b6b9084b514737
AndreuCi <carola.cfg@gmail.com>
2025-08-06 19:13:04 -0600

    se a creado el archivo

---
+++ Trabajo1.py
@@ -0,0 +1,7 @@
+import random
+long=int(input("de cuantos caracteres quieres tu contraseña"))
+contraseña=""
+letras="+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+for i in range(long):
+    contraseña += random.choice(letras)
+print(contraseña)
