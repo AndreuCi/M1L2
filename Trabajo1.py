72a4608bbf736e6772e50be820f77cbfebc81ffa
AndreuCi <carola.cfg@gmail.com>
2025-08-06 19:15:36 -0600

    con funcion

--- Trabajo1.py
+++ Trabajo1.py
@@ -1,7 +1,9 @@
 import random
-long=int(input("de cuantos caracteres quieres tu contraseña"))
-contraseña=""
-letras="+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-for i in range(long):
-    contraseña += random.choice(letras)
-print(contraseña)
+def generador():
+    long=int(input("de cuantos caracteres quieres tu contraseña"))
+    contraseña=""
+    letras="+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+    for i in range(long):
+        contraseña += random.choice(letras)
+    print(contraseña)
+generador()
